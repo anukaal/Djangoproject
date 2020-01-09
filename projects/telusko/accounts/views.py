cdc1224caff7a8c4b2fe44929e6f818d793be0d1
anurag <mailanu98@gmail.com>
2020-01-09 23:02:39 +0530

    feat:Login and Logout

--- projects/telusko/accounts/views.py
+++ projects/telusko/accounts/views.py
@@ -3,6 +3,22 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 
+def login(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = auth.authenticate(username=username,password=password)
+
+		if user is not None:
+			auth.login(request,user)
+			return redirect("/")
+		else:
+			messages.info(request,'invalid credentials')	
+			return redirect('login')
+
+	else:
+		return render(request,'login.html')
 
 def register(request):
 
@@ -26,9 +42,17 @@ def register(request):
 				user = User.objects.create_user(username=username, password=password1, email=email , first_name=first_name, last_name=last_name)
 				user.save();
 				print('user created')
+				return redirect('login')
+
 		else:
 			messages.info(request,'password not mactching...')
 			return redirect('register')
 		return redirect('/')	
 	else:
 		return render(request,'register.html')
+
+
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
